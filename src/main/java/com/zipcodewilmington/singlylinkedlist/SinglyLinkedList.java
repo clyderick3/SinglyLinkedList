d6384dc6e99b71ae5576ecc70f4f32b509ef7835
Clyde Broderick <clyderick3@gmail.com>
2022-05-08 22:12:41 -0400

    Added an add method.

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,22 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    static LinkedList<String> linky = new LinkedList<String>();
+
+    public static void addList(){
+        linky.add("Sarah");
+        linky.add("Samuel");
+        linky.add("Susan");
+
+    }
+
+    public static void main(String[] args) {
+        addList();
+        System.out.println(linky);
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,13 +1,16 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
     //Starting LinkedListTest.
     //Given
-
+    LinkedList linky = new LinkedList();
     //When
 
     //Then
+
 }
